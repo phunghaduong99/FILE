996aa4a1f4d23ab57058a7127d5e10a95c4a2652
phunghaduong99 <33689476+phunghaduong99@users.noreply.github.com>
2018-05-29 00:49:27 +0700

    Update Duong.cpp

--- Duong.cpp
+++ Duong.cpp
@@ -11,25 +11,23 @@ int main()
 	while(q ==1)
 		{
 		q=fscanf(fp, "%d", &number[i]);
-//Ham fscanf neu thanh cong tra se ve gia tri q= 1. 
-//		printf("%d\n", buff[i] );
-//		printf("  %d \t ",q );
+//Ham fscanf neu thanh cong tra se ve gia tri q=1. 
 		fseek( fp , 1L, SEEK_CUR);
 //Ham di chuyen con tro trong file.
 		i++;
 		p++;
-//p l‡ so cac so co trong file.
+//p l√† so cac so co trong file.
 		}
 	fclose(fp);
 	
 //Tinh tong cac so.
 	int tong=0;
-	for(int i=0; i<=p ; i++)
+	for(int i=0; i<=p-1 ; i++)
 		{
 		tong= tong+ number[i];	
 		}
 
-//Xuat ra file tong cac so
+//Xuat ra file tong cac so.
 	FILE *in;
 	in = fopen("FILE2.txt","w+");
 	fprintf(in, "%d", tong);
